a36af7ff1b878b4e54b6e452a6640e983e6b5785
jomino <github@nc.seltsam.net>
2024-12-01 01:00:42 +0100

    Rewrote update script

--- scripts/update.zsh
+++ scripts/update.zsh
@@ -1,25 +1,15 @@
 #!/bin/zsh
-while true; do 
-    read "?Do you want to update? (y/n) " yn
-    case $yn in 
-	     [Yy]*) 
-            echo ok updating
-            sudo apt-get update
-            sudo apt-get full-upgrade
-            sudo apt-get autoremove
-            sudo apt-get autoclean
-            sudo apt-get upgrade
-            sudo apt-get autoremove
-            brew update
-            brew outdated
-            brew upgrade
-            brew cleanup
-            mamba upgrade all
-            mamba clean --all --yes
-            tlmgr update --self --all --reinstall-forcibly-removed
-            break;;
-	     [Nn]*) 
-            echo Sure if you say so. Update then...
-            break;;
-    esac
-done
+echo "Running update Script"
+sudo apt-get update
+sudo apt-get full-upgrade
+sudo apt-get autoremove
+sudo apt-get autoclean
+sudo apt-get upgrade
+sudo apt-get autoremove
+brew update
+brew outdated
+brew upgrade
+brew cleanup
+mamba upgrade all
+mamba clean --all --yes
+tlmgr update --self --all --reinstall-forcibly-removed
