25f9451cb1f5c3938befceb7588c0f8c849d4978
Jonas Minor <github@nc.seltsam.net>
2025-02-26 13:00:33 +0100

    Updated Font definition for Weztem
    This requires updating to the Monaspace 1.2 Family as new features are
    used.

--- .homedir/.config/wezterm/wezterm.lua
+++ .homedir/.config/wezterm/wezterm.lua
@@ -18,30 +18,7 @@ end
 config.default_domain = "WSL:Ubuntu"
 config.term = "wezterm"
 config.color_scheme = "Catppuccin Mocha" -- or Macchiato, Frappe, Latte, nord
-config.font = wezterm.font_with_fallback({
-    {
-        family = "MonaspiceNe NFM Medium",
-        weight = "Regular",
-        italic = false,
-        scale = 1, -- The code below activates font ligatures
-        harfbuzz_features = {
-            "liga=1",
-            "dlig=1",
-            "calt=1",
-            "clig=1",
-            "ss01=1",
-            "ss02=1",
-            "ss03=1",
-            "ss04=1",
-            "ss05=1",
-            "ss06=1",
-            "ss07=1",
-            "ss08=1",
-        },
-    },
-    { family = "MesloLGS NF", scale = 1 },
-    { family = "SourceCodePro+Powerline+Awesome+Regular", scale = 1 },
-})
+config.font = wezterm.font(app.font)
 config.adjust_window_size_when_changing_font_size = false
 config.font_size = 14
 config.line_height = 1
